57118b36355e5f8883d27719aa1529337e0b648b
berk-karaal <karaalberk7@gmail.com>
2024-08-09 18:36:37 +0300

    feat(cmd): implement "clean" command

--- cmd/clean.go
+++ cmd/clean.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
-
+	"github.com/berk-karaal/todo-cli/internal/database"
+	"github.com/berk-karaal/todo-cli/internal/repository"
 	"github.com/spf13/cobra"
+	"time"
 )
 
 // cleanCmd represents the clean command
@@ -22,6 +24,28 @@ func init() {
 }
 
 func commandClean(cmd *cobra.Command, args []string) error {
-	fmt.Println("clean called")
+	db, err := database.NewDB()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	dayFlag, err := cmd.Flags().GetInt("day")
+	if err != nil {
+		return err
+	}
+
+	now := time.Now()
+	createdAtLimit := time.Date(
+		now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local,
+	).AddDate(0, 0, -dayFlag)
+
+	todoRepo := repository.NewTodoRepository(db)
+	deletedTodoCount, err := todoRepo.DeleteTodoByCreatedAtSmallerThan(createdAtLimit)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Deleted %d todos created before %s.\n", deletedTodoCount, createdAtLimit.Format(time.DateOnly))
 	return nil
 }
