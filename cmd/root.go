fc2958b878685bc113162a6184fc036bca3d9161
berk-karaal <karaalberk7@gmail.com>
2024-08-09 16:04:13 +0300

    feat(cmd): initialize viper on app start

--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,21 @@
 package cmd
 
 import (
-	"os"
-
+	"errors"
+	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+	"path"
 )
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "todo",
 	Short: "Simple todo cli app for daily tasks",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		return initViper()
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -22,4 +28,35 @@ func Execute() {
 }
 
 func init() {
+}
+
+func initViper() error {
+	userConfigDir, err := os.UserConfigDir()
+	if err != nil {
+		return err
+	}
+
+	configDir := path.Join(userConfigDir, "todo-cli")
+
+	err = os.MkdirAll(configDir, 0766)
+	if err != nil {
+		return err
+	}
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(configDir)
+	if err := viper.ReadInConfig(); err != nil {
+		var configFileNotFoundError viper.ConfigFileNotFoundError
+		if errors.As(err, &configFileNotFoundError) {
+			// create a new config file with default settings
+			viper.SetDefault("database.location", "./todo.sqlite")
+
+			err = viper.WriteConfigAs(path.Join(configDir, "config.yaml"))
+			if err != nil {
+				return fmt.Errorf("unable to write config file: %w", err)
+			}
+		}
+	}
+	return nil
 }
